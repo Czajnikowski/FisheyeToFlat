c9f84c8f37487549597b5d79dcd78cefb27a5fb5
Maciek Czarnik <czajnikowski@gmail.com>
2024-04-25 12:50:39 +0200

    Initial

---
+++ LaciFisheye/LaciFisheyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaciFisheyeApp.swift
+//  LaciFisheye
+//
+//  Created by Maciek Czarnik on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LaciFisheyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
